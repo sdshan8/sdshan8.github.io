f8d8cbc624c116492f772ab4f911c56001aec3f4
SDShan8 <sdshan8@gmail.com>
2024-07-01 15:59:25 +0530

    Updated Header

---
+++ src/lib/Header.svelte
@@ -0,0 +1,6 @@
+<header>
+    <a href="/"><i class="bi bi-house"></i> Home</a>
+    <!-- <a href="/about.html"><i class="bi bi-info-circle"></i> About</a>
+    <a href="/projects.html"><i class="bi bi-code-slash"></i> Projects</a> -->
+    <a href="/socials.html"><i class="bi bi-chat"></i> Socials</a>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,10 @@
 <script>
-
+import Header from "$lib/Header.svelte";
 
 </script>
-<header>
-    <a href="/"><i class="bi bi-house"></i> Home</a>
-    <a href="/about.html"><i class="bi bi-info-circle"></i> About</a>
-    <a href="/projects.html"><i class="bi bi-code-slash"></i> Projects</a>
-    <a href="/socials.html"><i class="bi bi-chat"></i> Socials</a>
-</header>
+<Header/>
 <main>
-    <slot></slot>
+    <slot/>
 </main>
 <style>
 @import '$lib/css/main.css';
