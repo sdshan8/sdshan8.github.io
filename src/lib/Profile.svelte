a6648f7a6b9786cfacaf14b5f97acec6461c277d
SDShan8 <sdshan8@gmail.com>
2024-07-02 22:08:00 +0530

    Added socials page

---
+++ src/lib/Profile.svelte
@@ -0,0 +1,75 @@
+<script>
+    export let profile;
+
+    const bg = profile.bg ? `url('${profile.bg}')` : "initial";
+    const bgColor = profile.bgColor || "initial";
+    const name = profile.name || "YutsuCard";
+    const avatar = profile.pic || "/icons/appple-touch-icon.png";
+    const username = profile.username || "unknown";
+    const link = profile.link || "https://sdshan8.github.io";
+    const bio = profile.bio || "";
+</script>
+
+<div class="profile" style:background-image={bg}>
+    <div class="con" style:background-color={bgColor}>
+        <div style="display:flex;flex-grow:1">
+            {#if link}
+                <a style="all:unset" target="_blank" href={link}>
+                    <img class="pfp" src={avatar} alt="{name}'s Profile Pic" />
+                </a>
+            {:else}
+                <img class="pfp" src={avatar} alt="{name}'s Profile Pic" />
+            {/if}
+
+            <div class="info">
+                {#if link}
+                    <a style="all:unset" target="_blank" href={link}>
+                        <b class="name">{name}</b>
+                    </a>
+                    <a style="all:unset" target="_blank" href={link}>
+                        <span class="username">{username}</span>
+                    </a>
+                {:else}
+                    <b class="name">{name}</b>
+                    <span class="username">{username}</span>
+                {/if}
+            </div>
+        </div>
+        <div class="bio">{bio}</div>
+    </div>
+</div>
+
+<style>
+    .profile {
+        border: 0.5px solid hsl(259, 12%, 50%);
+        border-radius: 20px;
+        margin-top: 0.2rem;
+        min-width: 350px;
+        background-size: cover;
+        background-position: center;
+    }
+    .con {
+        display: flex;
+        padding: 0.5rem;
+        flex-direction: column;
+        border-radius: 20px;
+    }
+    .pfp {
+        flex-shrink: 0;
+        width: 3.2rem;
+        height: 3.2rem;
+        border-radius: 50%;
+        border: 0.5px solid hsl(259, 12%, 50%);
+    }
+    .info {
+        display: flex;
+        flex-direction: column;
+        font-size: 16px;
+        margin-bottom: 0.2rem;
+        margin-left: 0.2rem;
+    }
+    .bio {
+        font-size: 0.8em;
+        margin-left: 0.3rem;
+    }
+</style>
---
+++ src/routes/social/+page.svelte
@@ -0,0 +1,87 @@
+<script>
+import Emote from '$lib/Emote.svelte';
+import Profile from '$lib/Profile.svelte';
+
+
+const socials = {
+    twitter: {
+        yutsu: {
+            name: "Yutsu",
+            pic: "/pfps/twitter/SetsuYuki4.jpg",
+            bg: "/pfps/twitter/SetsuYuki4_banner.jpg",
+            bgColor: "#00000088",
+            username: "SetsuYuki4",
+            link: "https://x.com/@SetsuYuki4",
+            bio: "He/Him | 19 | Kiana Simp | Likes Coding sometimes | AyuSetsu is life | likes cute things | Watch/Read DaL"
+        },
+        hibiki: {
+            name: "Hibiki",
+            pic: "/pfps/twitter/Hibiki146.jpg",
+            bg: "/pfps/twitter/Hibiki146_banner.jpg",
+            bgColor: "#00000088",
+            username: "Hibiki14607682",
+            link: "https://x.com/@Hibiki14607682",
+            bio: "He/Him"
+        }
+    },
+    discord: {
+        yutsu: {
+            name: "Yutsu",
+            pic: "/pfps/discord/Hibiki146.jpg",
+            bg: "/pfps/discord/Hibiki146_banner.jpg",
+            bgColor: "#00000088",
+            username: "hibiki146",
+            bio: "He/Him | 19 | Kiana Simp | Likes Coding sometimes | AyuSetsu is life | likes cute things | Watch/Read DaL"
+        }
+    },
+    github: {
+        shan: {
+            name: "Shan",
+            pic: "/pfps/github/sdshan8.jpg",
+            bg: "/pfps/github/sdshan8_banner.jpg",
+            bgColor: "#00000088",
+            username: "sdshan8",
+            link: "https://github.com/sdshan8",
+            bio:"Hello There"
+        }
+    }
+
+}
+</script>
+
+<svelte:head>
+    <title>YutSocial</title>
+</svelte:head>
+
+<div id="socials">
+    <h2>So this is the page in which I will put all my socials <Emote emote="BocchiExistentialCrisis"></Emote></h2>
+    <section id="Twitters">
+        <p>So lets start with my Twitter <i class="bi bi-twitter"></i> accounts:</p>
+        <Profile profile={socials.twitter.yutsu}></Profile>
+        
+        <Profile profile={socials.twitter.hibiki}></Profile>
+    </section>
+    <section id="Discords">
+        <p>Lets move on to my Discord <i class="bi bi-discord"></i> accounts:</p>
+        <Profile profile={socials.discord.yutsu}></Profile> 
+    </section>
+    <section id="Github">
+        <p>Now for my Github <i class="bi bi-github"></i> accounts:</p>
+        <Profile profile={socials.github.shan}></Profile>
+    </section>
+</div>
+
+<style>
+#socials {
+    margin: auto;
+    max-width: 800px;
+    /* padding: 1em; */
+    height: 100%;
+}
+h2 {
+    text-align: center;
+}
+section {
+    font-size: 1.2em;
+}
+</style>
