9ba2e4cbc3e8ace05e1a053b9fc7438faf8b9c97
SDShan8 <sdshan8@gmail.com>
2024-07-02 22:45:59 +0530

    CI/CD using github actions

--- svelte.config.js
+++ svelte.config.js
@@ -6,12 +6,13 @@ const config = {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'docs',
-			assets: 'docs',
 			fallback: '404.html',
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
