80202c2906096ee89da1be97bd34df53203c4211
SDShan8 <sdshan8@gmail.com>
2024-07-03 18:27:57 +0530

    Footer

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,19 @@
+<footer>
+    <p>The repo for this website can be found at <a href="https://github.com/sdshan8/sdshan8.github.io/">github.com/sdshan8/sdshan8.github.io</a>.</p>
+</footer>
+<style>
+footer {
+    width: 100%;
+    height: 2rem;
+    display: flex;
+    font-size: smaller;
+    background-color: var(--footer-bg);
+    color: var(--footer-accent);
+    align-items: center;
+    justify-content: center;
+    justify-items: center;
+}
+a {
+    color: var(--footer-link);
+}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
 import Header from "$lib/Header.svelte";
+import Footer from "$lib/Footer.svelte";
 </script>
 <Header/>
 <main>
     <slot/>
 </main>
+<Footer/>
 <style>
 @import '$lib/css/main.css';
 main {
