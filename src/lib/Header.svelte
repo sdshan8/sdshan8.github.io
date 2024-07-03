27ac9757918dc36ccc3e13233a40065dd5452e66
SDShan8 <sdshan8@gmail.com>
2024-07-03 16:40:57 +0530

    Moved styles from `main.css` to `Header.svelte` Fixed the home link not working

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,13 +3,34 @@
 </script>
 
 <header>
-    <a href={base}><i class="bi bi-house"></i> Home</a>
-    <!-- <a href="/about"><i class="bi bi-info-circle"></i> About</a>
-    <a href="/projects"><i class="bi bi-code-slash"></i> Projects</a> -->
+    <a href={base + "/"}><i class="bi bi-house"></i> Home</a>
+    <!-- <a href={base + '/about'}><i class="bi bi-info-circle"></i> About</a>
+    <a href={base + '/projects'}><i class="bi bi-code-slash"></i> Projects</a> -->
     <a href={base + "/social"}><i class="bi bi-chat"></i> Socials</a>
 </header>
+
 <style>
-header {
-    border-bottom: 0.5px solid var(--lavender-blue-50);
-}
+    header {
+        border-bottom: 0.5px solid var(--lavender-blue-50);
+        display: flex;
+        justify-content: space-evenly;
+        width: 100%;
+    }
+    header > a {
+        color: var(--header-accent);
+        justify-content: center;
+        flex-basis: 100%;
+        display: flex;
+        padding: 1em 1em;
+        transition:
+            background-color 200ms,
+            color 200ms,
+            flex-basis 200ms;
+    }
+
+    header > a:hover {
+        flex-basis: 150%;
+        color: var(--backgroung);
+        background-color: var(--header-accent);
+    }
 </style>
