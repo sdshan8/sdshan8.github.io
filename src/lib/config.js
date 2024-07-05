ddac953e75c2f61fb11f7d05c5fd01e8879e4cef
SDShan8 <sdshan8@gmail.com>
2024-07-05 19:44:11 +0530

    Updated the socials to use a config file

---
+++ src/lib/config.js
@@ -0,0 +1,62 @@
+const social_page = {
+    description: "So this is the page in which I will put all my socials",
+    emote: "BocchiExistentialCrisis",
+    socials: [
+        {
+            name: "twitter",
+            description: "So lets start with my Twitter accounts",
+            list: [
+                {
+                    name: "Yutsu",
+                    pic: "/pfps/twitter/SetsuYuki4.jpg",
+                    bg: "/pfps/twitter/SetsuYuki4_banner.jpg",
+                    bgColor: "#00000088",
+                    username: "SetsuYuki4",
+                    link: "https://x.com/@SetsuYuki4",
+                    bio: "He/Him | 19 | Kiana Simp | Likes Coding sometimes | AyuSetsu is life | likes cute things | Watch/Read DaL"
+                },
+                {
+                    name: "Hibiki",
+                    pic: "/pfps/twitter/Hibiki146.jpg",
+                    bg: "/pfps/twitter/Hibiki146_banner.jpg",
+                    bgColor: "#00000088",
+                    username: "Hibiki14607682",
+                    link: "https://x.com/@Hibiki14607682",
+                    bio: "He/Him"
+                }
+            ]
+        },
+        {
+            name: "discord",
+            description: "Lets move on to my Discord accounts",
+            list: [
+                {
+                    name: "Yutsu",
+                    pic: "/pfps/discord/Hibiki146.jpg",
+                    bg: "/pfps/discord/Hibiki146_banner.jpg",
+                    bgColor: "#00000088",
+                    username: "hibiki146",
+                    link: "https://discordid.netlify.app/?id=1043219924962783262",
+                    bio: "He/Him | 19 | Kiana Simp | Likes Coding sometimes | AyuSetsu is life | likes cute things | Watch/Read DaL"
+                }
+            ]
+        },
+        {
+            name: "github",
+            description: "Now for my Github accounts",
+            list: [
+                {
+                    name: "Shan",
+                    pic: "/pfps/github/sdshan8.jpg",
+                    bg: "/pfps/github/sdshan8_banner.jpg",
+                    bgColor: "#00000088",
+                    username: "sdshan8",
+                    link: "https://github.com/sdshan8",
+                    bio:"Hello There"
+                }
+            ]
+        }
+    ]
+}
+
+export { social_page }
--- src/routes/social/+page.svelte
+++ src/routes/social/+page.svelte
@@ -1,53 +1,9 @@
 <script>
 import Emote from '$lib/Emote.svelte';
 import Profile from '$lib/Profile.svelte';
+import { social_page } from '$lib/config.js'
 
 
-const socials = {
-    twitter: {
-        yutsu: {
-            name: "Yutsu",
-            pic: "/pfps/twitter/SetsuYuki4.jpg",
-            bg: "/pfps/twitter/SetsuYuki4_banner.jpg",
-            bgColor: "#00000088",
-            username: "SetsuYuki4",
-            link: "https://x.com/@SetsuYuki4",
-            bio: "He/Him | 19 | Kiana Simp | Likes Coding sometimes | AyuSetsu is life | likes cute things | Watch/Read DaL"
-        },
-        hibiki: {
-            name: "Hibiki",
-            pic: "/pfps/twitter/Hibiki146.jpg",
-            bg: "/pfps/twitter/Hibiki146_banner.jpg",
-            bgColor: "#00000088",
-            username: "Hibiki14607682",
-            link: "https://x.com/@Hibiki14607682",
-            bio: "He/Him"
-        }
-    },
-    discord: {
-        yutsu: {
-            name: "Yutsu",
-            pic: "/pfps/discord/Hibiki146.jpg",
-            bg: "/pfps/discord/Hibiki146_banner.jpg",
-            bgColor: "#00000088",
-            username: "hibiki146",
-            link: "https://discordid.netlify.app/?id=1043219924962783262",
-            bio: "He/Him | 19 | Kiana Simp | Likes Coding sometimes | AyuSetsu is life | likes cute things | Watch/Read DaL"
-        }
-    },
-    github: {
-        shan: {
-            name: "Shan",
-            pic: "/pfps/github/sdshan8.jpg",
-            bg: "/pfps/github/sdshan8_banner.jpg",
-            bgColor: "#00000088",
-            username: "sdshan8",
-            link: "https://github.com/sdshan8",
-            bio:"Hello There"
-        }
-    }
-
-}
 </script>
 
 <svelte:head>
@@ -55,21 +11,15 @@ const socials = {
 </svelte:head>
 
 <div id="socials">
-    <h2>So this is the page in which I will put all my socials <Emote emote="BocchiExistentialCrisis"></Emote></h2>
-    <section id="Twitters">
-        <p>So lets start with my Twitter <i class="bi bi-twitter"></i> accounts:</p>
-        <Profile profile={socials.twitter.yutsu}></Profile>
-        
-        <Profile profile={socials.twitter.hibiki}></Profile>
+    <h2>{social_page.description}<Emote emote={social_page.emote} /></h2>
+    {#each social_page.socials as social }
+    <section id={social.name + "-socials"}>
+        <p>So lets start with my Twitter <i class={'"bi bi-'+social.name+'"'}></i></p>
+        {#each social.list as profile}
+            <Profile profile={profile}></Profile>
+	    {/each}
     </section>
-    <section id="Discords">
-        <p>Lets move on to my Discord <i class="bi bi-discord"></i> accounts:</p>
-        <Profile profile={socials.discord.yutsu}></Profile> 
-    </section>
-    <section id="Github">
-        <p>Now for my Github <i class="bi bi-github"></i> accounts:</p>
-        <Profile profile={socials.github.shan}></Profile>
-    </section>
+    {/each}
 </div>
 
 <style>
